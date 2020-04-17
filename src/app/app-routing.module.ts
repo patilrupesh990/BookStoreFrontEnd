a5965e9112e8a6409d1e48ab2af83e9ae624bb8c
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-17 22:56:34 +0530

    [Rupesh Patil].updated Routing.ts File

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RegistrationComponent } from "./component/authentication/registration/registration.component";
+import { UserLoginComponent } from "./component/authentication/user-login/user-login.component";
+import { DashboardComponent } from "./component/dashboard/dashboard.component";
+import { DisplaybooksComponent } from "./component/displaybooks/displaybooks.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "register", component: RegistrationComponent },
+  { path: "", component: UserLoginComponent },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    children: [{ path: "", component: DisplaybooksComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
