1a0a015f87e96934d0f6ef237811f12abd6d816f
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-17 22:58:48 +0530

    [Rupesh Patil].updated app module ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,73 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RegistrationComponent } from "./component/authentication/registration/registration.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatCardTitle,
+  MatSnackBar,
+  MatSnackBarModule,
+  MatOption,
+  MatOptionModule,
+  MatSelectModule,
+} from "@angular/material";
+import { ReactiveFormsModule, FormBuilder, FormsModule } from "@angular/forms";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { HttpClientModule } from "@angular/common/http";
+import { UserLoginComponent } from "./component/authentication/user-login/user-login.component";
+import { ToolbarComponent } from "./component/toolbar/toolbar.component";
+import { DashboardComponent } from "./component/dashboard/dashboard.component";
+import { DisplaybooksComponent } from "./component/displaybooks/displaybooks.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    UserLoginComponent,
+    ToolbarComponent,
+    DashboardComponent,
+    DisplaybooksComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCardModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    NgxSpinnerModule,
+    FlexLayoutModule,
+    MatTooltipModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+// tslint:disable-next-line: one-line
+export class AppModule {}
