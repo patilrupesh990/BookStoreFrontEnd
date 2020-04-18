8218f39a475ab4e4a27760b119c6e18e762a1b3e
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-19 04:15:21 +0530

    [Rupesh Patil].Updated Environment .ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,17 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  production: false,
+  userApiUrl: "http://localhost:8092/users",
+  registerURL: "register",
+  userLoginUrl: "login",
+  sellerLoginUrl: "seller-login",
+  sellerRegister: "seller-register",
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  bookApiUrl: "http://localhost:8091/books",
+  getBooksList: "getAllBooks",
+  getSellerBookList: "sellerBooks",
+  addbook: "add",
+  deleteBook: "delete",
+
+  cartApiUrl: "http://localhost:8091/orders",
+  addToBag: "make-order",
+};
