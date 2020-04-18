f2e853c300587a82da96c8517153546be640a3a7
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-19 04:10:55 +0530

    [Rupesh patil].added book model class

--- src/app/component/toolbar/toolbar.component.ts
+++ src/app/component/toolbar/toolbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  selector: "app-toolbar",
+  templateUrl: "./toolbar.component.html",
+  styleUrls: ["./toolbar.component.scss"],
 })
 export class ToolbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  name: any;
+  constructor() {
+    this.name = sessionStorage.firstName + sessionStorage.lastName;
   }
 
+  ngOnInit() {}
+
+  onClickClear() {
+    sessionStorage.clear();
+    localStorage.clear();
+  }
 }
---
+++ src/app/shared/model/book.model.ts
@@ -0,0 +1,10 @@
+export class Book {
+  bookName: string;
+  bookId: number;
+  userId: number;
+  bookCode: string;
+  bookDetails: string;
+  price: number;
+  quantity: number;
+  authorName: string;
+}
