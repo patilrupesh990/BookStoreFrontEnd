50860a089a69309c92aa681e9a620a80d904c519
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-19 04:12:51 +0530

    [Rupesh Patil].added Icons in Assets

--- src/app/shared/service/user.service.ts
+++ src/app/shared/service/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { environment } from "../../../environments/environment";
 import { HttpService } from "./http.service";
 
@@ -8,14 +8,25 @@ import { HttpService } from "./http.service";
   providedIn: "root",
 })
 export class UserService {
+  private queryParam = new Subject<any>();
+
   private httpOtions = {
     headers: new HttpHeaders({ "content-type": "application/json" }),
   };
   constructor(private http: HttpClient, private httpservice: HttpService) {}
+
   registerUser(user: any): Observable<any> {
     console.log("User Email", user.email);
     return this.httpservice.post(
       `${environment.userApiUrl}/${environment.registerURL}`,
+      user,
+      this.httpOtions
+    );
+  }
+
+  registerSeller(user: any): Observable<any> {
+    return this.httpservice.post(
+      `${environment.userApiUrl}/${environment.sellerRegister}`,
       user,
       this.httpOtions
     );
@@ -28,5 +39,21 @@ export class UserService {
       user,
       { headers: new HttpHeaders().set("password", user.password) }
     );
+  }
+
+  loginAdmin(user: any): Observable<any> {
+    return this.httpservice.post(
+      `${environment.userApiUrl}/${environment.sellerLoginUrl}/${user.email}`,
+      user,
+      { headers: new HttpHeaders().set("password", user.password) }
+    );
+  }
+
+  setQueryParam(message: any) {
+    this.queryParam.next({ id: message });
+  }
+
+  getQueryParam(): Observable<any> {
+    return this.queryParam.asObservable();
   }
 }
