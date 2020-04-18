8983483f4a410f8ffaeb20d0ccf83423c3b0692f
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-19 04:06:42 +0530

    [Rupesh Patil].Updated Display Component  With Seller Add Update Delete Book Module

--- src/app/component/displaybooks/displaybooks.component.ts
+++ src/app/component/displaybooks/displaybooks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { MatDialog, MatSnackBar, TooltipPosition } from "@angular/material";
 import { FormBuilder, FormControl } from "@angular/forms";
 import { Router, ActivatedRoute } from "@angular/router";
@@ -6,6 +6,10 @@ import { HttpClient } from "@angular/common/http";
 import { BookService } from "src/app/shared/service/book.service";
 import { Message } from "@angular/compiler/src/i18n/i18n_ast";
 import { environment } from "src/environments/environment";
+import { UserService } from "src/app/shared/service/user.service";
+import { AddbookComponent } from "../addbook/addbook.component";
+import { CartServiceService } from "src/app/shared/service/cart-service.service";
+import { UpdateBookComponent } from "../update-book/update-book.component";
 
 @Component({
   selector: "app-displaybooks",
@@ -15,6 +19,11 @@ import { environment } from "src/environments/environment";
 export class DisplaybooksComponent implements OnInit {
   books: any;
   size: any;
+  id: any;
+  isUser = false;
+  isSeller = false;
+  toggle = true;
+
   constructor(
     private dialog: MatDialog,
     private formBuilder: FormBuilder,
@@ -22,19 +31,78 @@ export class DisplaybooksComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private bookService: BookService,
-    private httpClient: HttpClient
-  ) {}
-
-  ngOnInit() {
-    this.getAllBookList();
+    private httpClient: HttpClient,
+    private userService: UserService,
+    private cartService: CartServiceService
+  ) {
+    this.userService.getQueryParam().subscribe((message) => {
+      this.id = message.id;
+      if (this.id === 1) {
+        this.isSeller = false;
+        this.isUser = true;
+        this.getAllBookList();
+      } else if (this.id === 2) {
+        this.isSeller = true;
+        this.getSellerBook();
+      }
+    });
   }
 
+  ngOnInit() {}
+
   getAllBookList() {
-    console.log(sessionStorage.token);
     this.bookService.getBookList().subscribe((message) => {
       console.log(message);
       this.books = message.bookList;
       this.size = message.bookList.length;
+    });
+  }
+
+  getSellerBook() {
+    this.bookService.getSellerBookList().subscribe((message) => {
+      console.log(message);
+      this.books = message.bookList;
+      this.size = message.bookList.length;
+    });
+  }
+  addBook() {}
+
+  deleteBook(bookId) {
+    this.bookService.deleteBook(bookId).subscribe((meassage) => {
+      this.matSnackBar.open("Book Deleted Successfully", "OK", {
+        duration: 4000,
+      });
+    });
+    this.matSnackBar.open("Error in Book Deletion", "ok", { duration: 4000 });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddbookComponent, {
+      width: "auto",
+      panelClass: "custom-dialog-container",
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
+    });
+  }
+
+  openDialog2(): void {
+    const dialogRef = this.dialog.open(UpdateBookComponent, {
+      width: "auto",
+      panelClass: "custom-dialog-container",
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
+    });
+  }
+
+  addToBag(bookId, quantity) {
+    this.toggle = !this.toggle;
+    this.cartService.addToBag(bookId, 1).subscribe((message) => {
+      console.log(message);
+      this.matSnackBar.open("Book Added to Bag SuccessFully", "OK", {
+        duration: 4000,
+      });
     });
   }
 }
