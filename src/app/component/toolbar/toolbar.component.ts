bf6e9cc3c78dff278c14f964ff9502306ad1f468
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-22 03:56:15 +0530

    [Rupesh Patil].Updated  toolBar Component

--- src/app/component/toolbar/toolbar.component.ts
+++ src/app/component/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { environment } from "src/environments/environment";
+import { UserService } from "src/app/shared/service/user.service";
 
 @Component({
   selector: "app-toolbar",
@@ -8,8 +9,20 @@ import { environment } from "src/environments/environment";
 })
 export class ToolbarComponent implements OnInit {
   name: any;
-  constructor() {
+  id: any;
+  isUser = false;
+  isSeller = false;
+  constructor(private userService: UserService) {
     this.name = sessionStorage.firstName + sessionStorage.lastName;
+    this.userService.getQueryParam().subscribe((message) => {
+      this.id = message.id;
+      if (this.id === 1) {
+        this.isSeller = false;
+        this.isUser = true;
+      } else if (this.id === 2) {
+        this.isSeller = true;
+      }
+    });
   }
 
   ngOnInit() {}
