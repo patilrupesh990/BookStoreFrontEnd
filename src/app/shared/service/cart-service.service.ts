76cf7cb224ab4bae0cb42766c4546dfb28153e31
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-24 00:16:34 +0530

    [Rupesh Patil].Updated cart service

--- src/app/shared/service/cart-service.service.ts
+++ src/app/shared/service/cart-service.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpService } from "./http.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { environment } from "src/environments/environment";
+import { tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
 })
 export class CartServiceService {
+  private _autoRefresh$ = new Subject();
+
+  get autoRefresh$() {
+    return this._autoRefresh$;
+  }
   constructor(private http: HttpClient, private httpservice: HttpService) {}
 
   addToBag(id, quantity): Observable<any> {
@@ -17,6 +23,18 @@ export class CartServiceService {
       { headers: new HttpHeaders().set("token", sessionStorage.token) }
     );
   }
+  removeFromeBag(id): Observable<any> {
+    return this.httpservice
+      .delete(
+        `${environment.cartApiUrl}/${environment.deleteOrder}?bookId=${id}`,
+        { headers: new HttpHeaders().set("token", sessionStorage.token) }
+      )
+      .pipe(
+        tap(() => {
+          this._autoRefresh$.next();
+        })
+      );
+  }
 
   getCartList(): Observable<any> {
     return this.httpservice.get(
@@ -24,5 +42,4 @@ export class CartServiceService {
       { headers: new HttpHeaders().set("token", sessionStorage.token) }
     );
   }
-  
 }
